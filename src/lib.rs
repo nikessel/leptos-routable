f25205ac98bdd6ac3782f1992fa2b5eed6fa75d8
geoffreygarrett <26066340+geoffreygarrett@users.noreply.github.com>
2025-01-13 12:30:53 +0200

    feat: hook extensions as feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+#[cfg(feature="hook-extensions")]
+mod hook_extensions;
 
 pub mod prelude {
+    #[cfg(feature="hook-extensions")]
+    pub use super::hook_extensions::*;
     pub use leptos_routable_macro::*;
 }
 
